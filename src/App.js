cc8f59665ab7a6d5b3c49cb3882aaa4aca2a8afb
5sidd <siddpcal@gmail.com>
2022-07-22 17:57:16 -0500

    Added wordle functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,315 @@
-import logo from './logo.svg';
 import './App.css';
+import femaleFighters from './female-fighters.json'
+import maleFighters from './male-fighters.json'
+import { useState } from 'react'
+
+let allFighters = femaleFighters.concat(maleFighters);
+for (let i = 0; i < allFighters.length; i++) {
+  allFighters[i].name = decodeURIComponent(allFighters[i].name).normalize('NFD').replace(/\p{Diacritic}/gu, "");
+}
+
+const weightClasses = ['Strawweight', 'Flyweight', 'Bantamweight', 'Featherweight', 'Lightweight', 'Welterweight', 'Middleweight', 'Light Heavyweight', 'Heavyweight'];
+const weightClassAbbreviations = ['SW', 'FLY', 'BW', 'FW', 'LW', 'WW', 'MW', 'LHW', 'HW'];
+
+const randomFighter = allFighters[Math.floor(Math.random() * allFighters.length)];
+
+const InputSection = (props) => {
+  const { onChange, guesses, found, history } = props;
+  console.log(history);
+
+  return (
+    <div>
+      <form>
+        <input type="text" style={{
+          display: 'block',
+          margin: 'auto',
+          minWidth: '310px',
+          width: '50%',
+          maxWidth: '1500px',
+          height: '50px',
+          borderColor: '#60CDEF',
+          marginTop: '100px',
+          fontSize: '25px'
+        }} onChange={onChange} placeholder={`Guess ${guesses + 1} of 5`} />
+      </form>
+    </div>
+  )
+}
+
+const SelectionButton = (props) => {
+  const { fighter, handleSelection } = props;
+
+  return (
+    <button style={{
+      display: 'block',
+      width: '100%',
+    }} onClick={handleSelection}>
+      {fighter.name}
+    </button>
+  )
+}
+
+const Guess = (props) => {
+  const { fighter, randomFighter } = props;
+  const { id, name, weightClass, rank, age, height, reach } = fighter;
+
+  let gender;
+  femaleFighters.find(f => f.id === id) ? gender = 'F' : gender = 'M';
+  const abbreviatedWeightClass = weightClassAbbreviations[weightClasses.findIndex(w => w === weightClass)];
+  const rankToShow = rank === 0 ? 'C' : rank;
+
+  let genderColor = (femaleFighters.find(f => f.id === fighter.id) && femaleFighters.find(f => f.id === randomFighter.id)) ||
+    (maleFighters.find(f => f.id === fighter.id) && maleFighters.find(f => f.id === randomFighter.id)) ? '#42E578' : '';
+
+  let weightClassColor;
+  let weightClassDifference = weightClasses.findIndex(w => w === weightClass) - weightClasses.findIndex(w => w === randomFighter.weightClass);
+  Math.abs(weightClassDifference) === 1 ? weightClassColor = '#DAEA48' : weightClassColor = '';
+
+  let weightClassText;
+  if (weightClassDifference === 0) {
+    weightClassText = abbreviatedWeightClass;
+    weightClassColor = '#42E578'
+  }
+  else if (weightClassDifference < 0) {
+    weightClassText = `${abbreviatedWeightClass} ${'▲'}`;
+  }
+  else {
+    weightClassText = `${abbreviatedWeightClass} ${'▼'}`
+  }
+
+  let rankColor;
+  let rankDifference = rank - randomFighter.rank;
+  Math.abs(rankDifference) === 2 || Math.abs(rankDifference) === 1 ? rankColor = '#DAEA48' : rankColor = '';
+
+  let rankText;
+  if (rankDifference === 0) {
+    rankText = `${rankToShow}`
+    rankColor = '#42E578'
+  }
+  else if (rankDifference < 0) {
+    rankText = `${rankToShow} ${'▼'}`;
+  }
+  else {
+    rankText = `${rankToShow} ${'▲'}`;
+  }
+
+  let heightColor;
+  let heightDifference = height - randomFighter.height;
+  Math.abs(heightDifference) === 2 || Math.abs(heightDifference) === 1 ? heightColor = '#DAEA48' : heightColor = '';
+
+  let heightText;
+  if (heightDifference === 0) {
+    heightText = `${height}"`
+    heightColor = '#42E578'
+  }
+  else if (heightDifference < 0) {
+    heightText = `${height}" ${'▲'}`;
+  }
+  else {
+    heightText = `${height}" ${'▼'}`;
+  }
+
+  let reachColor;
+  let reachDifference = reach - randomFighter.reach;
+  Math.abs(reachDifference) === 2 || Math.abs(reachDifference) === 1 ? reachColor = '#DAEA48' : reachColor = '';
+
+  let reachText;
+  if (reachDifference === 0) {
+    reachText = `${reach}"`
+    reachColor = '#42E578'
+  }
+  else if (reachDifference < 0) {
+    reachText = `${reach}" ${'▲'}`;
+  }
+  else {
+    reachText = `${reach}" ${'▼'}`;
+  }
+
+  let ageColor;
+  let ageDifference = age - randomFighter.age;
+  Math.abs(ageDifference) === 2 || Math.abs(ageDifference) === 1 ? ageColor = '#DAEA48' : ageColor = '';
+
+  let ageText;
+  if (ageDifference === 0) {
+    ageText = `${age}`
+    ageColor = '#42E578'
+  }
+  else if (ageDifference < 0) {
+    ageText = `${age} ${'▲'}`;
+  }
+  else {
+    ageText = `${age} ${'▼'}`;
+  }
+
+  return (
+    <div>
+      <h4 style={{ marginTop: '30px', width: '100%' }}> {name} </h4>
+      <table style={{ width: '100%' }}>
+        <tbody>
+          <tr>
+            <th style={{ backgroundColor: genderColor }}> M/F </th>
+            <th style={{ backgroundColor: weightClassColor }}> WC </th>
+            <th style={{ backgroundColor: rankColor }}> # </th>
+            <th style={{ backgroundColor: heightColor }}> HT </th>
+            <th style={{ backgroundColor: reachColor }}> Reach </th>
+            <th style={{ backgroundColor: ageColor }}> Age </th>
+          </tr>
+          <tr>
+            <td style={{ backgroundColor: genderColor }}> {gender} </td>
+            <td style={{ backgroundColor: weightClassColor }}> {weightClassText} </td>
+            <td style={{ backgroundColor: rankColor }}> {rankText} </td>
+            <td style={{ backgroundColor: heightColor }}> {heightText} </td>
+            <td style={{ backgroundColor: reachColor }}> {reachText} </td>
+            <td style={{ backgroundColor: ageColor }}> {ageText} </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
 function App() {
+  const [displayedFighters, setDisplayedFighters] = useState([]);
+  const [guessesNumber, setGuessNumber] = useState(0);
+  const [guesses, setGuesses] = useState([]);
+  const [found, setFound] = useState(false);
+
+  function onChange(event) {
+    console.log(event.target.value);
+    if (event.target.value !== '') {
+      let filteredFighters = allFighters.filter(fighter => event.target.value.trim().toLowerCase() === fighter.name.trim().toLowerCase().substring(0, event.target.value.trim().length));
+      if (filteredFighters.length > 5) {
+        filteredFighters = filteredFighters.splice(0, 5);
+      }
+
+      console.log(filteredFighters);
+      setDisplayedFighters(filteredFighters);
+    }
+    else {
+      setDisplayedFighters([])
+    }
+  }
+
+  if (guessesNumber > 5) {
+    console.log('Limit reached!!!!!');
+  }
+
+  if (found) {
+    return (
+      <>
+        <h2 style={{ width: '50%', minWidth: '310px', maxWidth: '1500px', margin: 'auto', marginTop: '25px', textAlign: 'center' }}> You guessed correctly! </h2>
+
+        <div>
+          <h4 style={{ width: '50%', minWidth: '310px', maxWidth: '1500px', margin: 'auto', marginTop: '30px', textAlign: 'center' }}> {randomFighter.name} </h4>
+          <table style={{ width: '50%', minWidth: '310px', maxWidth: '1500px', margin: 'auto', marginTop: '10px' }}>
+            <tbody>
+              <tr>
+                <th> M/F </th>
+                <th> WC </th>
+                <th> # </th>
+                <th> HT </th>
+                <th> Reach </th>
+                <th> Age </th>
+              </tr>
+              <tr>
+                <td> {femaleFighters.find(f => f.id === randomFighter.id) ? 'F' : 'M'} </td>
+                <td> {weightClassAbbreviations[weightClasses.findIndex(w => w === randomFighter.weightClass)]} </td>
+                <td> {randomFighter.rank} </td>
+                <td> {randomFighter.height}" </td>
+                <td> {randomFighter.reach} </td>
+                <td> {randomFighter.age} </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <h2 style={{ width: '50%', minWidth: '310px', maxWidth: '1500px', margin: 'auto', marginTop: '25px', textAlign: 'center' }}> Your guesses... </h2>
+
+        <div style={{ width: '50%', minWidth: '310px', maxWidth: '1500px', margin: 'auto' }}>
+          {guesses.map((guess) => {
+            return <Guess key={guess.id} fighter={guess} randomFighter={randomFighter} />
+          })}
+        </div>
+
+        <div style={{ height: '70px' }}></div>
+      </>
+    )
+  }
+
+  if (guessesNumber === 5) {
+    return (
+      <>
+        <h2 style={{ width: '50%', minWidth: '310px', maxWidth: '1500px', margin: 'auto', marginTop: '25px', textAlign: 'center' }}> You used all your guesses! </h2>
+        <p style={{ width: '50%', minWidth: '310px', maxWidth: '1500px', margin: 'auto', marginTop: '15px', textAlign: 'center' }}> Correct fighter was... </p>
+
+        <div>
+          <h4 style={{ width: '50%', minWidth: '310px', maxWidth: '1500px', margin: 'auto', marginTop: '30px', textAlign: 'center' }}> {randomFighter.name} </h4>
+          <table style={{ width: '50%', minWidth: '310px', maxWidth: '1500px', margin: 'auto', marginTop: '20px' }}>
+            <tbody>
+              <tr>
+                <th> M/F </th>
+                <th> WC </th>
+                <th> # </th>
+                <th> HT </th>
+                <th> Reach </th>
+                <th> Age </th>
+              </tr>
+              <tr>
+                <td> {femaleFighters.find(f => f.id === randomFighter.id) ? 'F' : 'M'} </td>
+                <td> {weightClassAbbreviations[weightClasses.findIndex(w => w === randomFighter.weightClass)]} </td>
+                <td> {randomFighter.rank} </td>
+                <td> {randomFighter.height}" </td>
+                <td> {randomFighter.reach} </td>
+                <td> {randomFighter.age} </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <h2 style={{ width: '50%', minWidth: '310px', maxWidth: '1500px', margin: 'auto', marginTop: '25px', textAlign: 'center' }}> Your guesses... </h2>
+        
+        <div style={{ width: '50%', minWidth: '310px', maxWidth: '1500px', margin: 'auto' }}>
+          {guesses.map((guess) => {
+            return <Guess key={guess.id} fighter={guess} randomFighter={randomFighter} />
+          })}
+        </div>
+
+        <div style={{ height: '70px' }}></div>
+      </>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <InputSection onChange={onChange} guesses={guessesNumber} found={found} history={guesses} />
+
+      <form style={{
+        width: '50%',
+        minWidth: '310px',
+        maxWidth: '1500px',
+        margin: 'auto',
+        marginTop: '30px',
+      }}>
+        {displayedFighters.map(fighter => {
+          return <SelectionButton key={fighter.id} fighter={fighter} handleSelection={(event) => {
+            event.preventDefault();
+            console.log(fighter);
+            console.log(randomFighter);
+            fighter.id === randomFighter.id ? setFound(true) : setFound(false);
+            setGuessNumber(guessesNumber + 1);
+            setGuesses(guesses.concat([fighter]));
+          }} />
+        })}
+      </form>
+
+      <div style={{ width: '50%', minWidth: '310px', maxWidth: '1500px', margin: 'auto' }}>
+        {guesses.map((guess) => {
+          return <Guess key={guess.id} fighter={guess} randomFighter={randomFighter} />
+        })}
+      </div>
+
+      <div style={{ height: '70px' }}></div>
+    </>
+  )
 }
 
 export default App;
---
+++ src/main.py
@@ -0,0 +1,166 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+
+rankings_page = requests.get('https://www.ufc.com/rankings')
+soup = BeautifulSoup(rankings_page.content, 'html.parser')
+
+headers = soup.find_all(class_="view-grouping")
+weight_classes = []
+
+i = 0
+while i < len(headers):
+    if (i != 0 and i != 9 and i != 13):
+        weight_classes.append(headers[i])
+    i = i + 1
+
+scraped_data_mens = []
+scraped_data_womens = []
+
+
+def process_weight_class(weight_class):
+    fighters_list = list(weight_class.find_all(class_='views-row'))
+    rankings = list(weight_class.find_all(class_='views-field-weight-class-rank'))
+
+    all_fighters = []
+    get_champion = weight_class.find('span')
+    champion_exists = True
+
+    if get_champion is None:
+        champion_exists = False
+        #print("Vacant Champion!")
+    
+    if champion_exists:
+        champion = {
+            "fighter": fighters_list[0].get_text(),
+            "link": fighters_list[0].find('a')['href'],
+            "rank": 0
+        }
+        all_fighters.append(champion)
+
+    if champion_exists:
+        i = 1
+        while i < len(fighters_list):
+            link = fighters_list[i].find('a')
+            fighter_object = {
+                "fighter": fighters_list[i].get_text(),
+                "link": link['href'],
+                "rank": int(rankings[i - 1].get_text())
+            }
+            all_fighters.append(fighter_object)
+            i = i + 1
+    else:
+        i = 0
+        while i < len(fighters_list):
+            link = fighters_list[i].find('a')
+
+            fighter_object = {
+                "fighter": fighters_list[i].get_text(),
+                "link": link['href'],
+                "rank": int(rankings[i].get_text())
+            }
+            all_fighters.append(fighter_object)
+            i = i + 1
+
+    weight_class_object = {
+        "weightClass": weight_class.find(class_='view-grouping-header').get_text(),
+        "fighters": all_fighters
+    }
+
+    return weight_class_object
+
+#print(process_weight_class(weight_classes[3]))
+
+def get_attributes(link):
+    athlete_page = requests.get('https://www.ufc.com' + link)
+    soup2 = BeautifulSoup(athlete_page.content, 'html.parser')
+    bio_data_raw = soup2.find_all(class_='c-bio__field')
+
+    age = 0
+    height = 0
+    reach = 0
+
+    for data in bio_data_raw:
+        if (data.find(class_='c-bio__label').get_text() == 'Age'):
+            age = int(data.find(class_='c-bio__text').get_text())
+
+        if (data.find(class_='c-bio__label').get_text() == 'Height'):
+            height = float(data.find(class_='c-bio__text').get_text())
+
+        if (data.find(class_='c-bio__label').get_text() == 'Reach'):
+            reach = float(data.find(class_='c-bio__text').get_text())
+
+    athlete_image = soup2.find(class_='c-bio__image').find('img')['src']
+
+    atttributes_object = {
+        "age": age,
+        "height": height,
+        "reach": reach,
+        "image": athlete_image
+    }
+
+    return atttributes_object
+
+def update_all():
+    i = 1
+    for w in weight_classes:
+        processed_weight_class = process_weight_class(w)
+        weight_class_fighters = processed_weight_class['fighters']
+
+        gender = 0
+        weight_class = ''
+
+        if processed_weight_class['weightClass'].split()[0] == "Women's":
+            gender = 0
+            weight_class = processed_weight_class['weightClass'].split()[1]
+        else:
+            gender = 1
+            weight_class = processed_weight_class['weightClass'].split()[0]
+
+        for f in weight_class_fighters:
+            bio_data = get_attributes(f['link'])
+            if gender == 0:
+                female_fighter = {
+                    "id": i,
+                    "name": f['fighter'],
+                    "weightClass": weight_class,
+                    "rank": f['rank'],
+                    "age": bio_data['age'],
+                    "height": bio_data['height'],
+                    "reach": bio_data['reach'],
+                    "image": bio_data['image']
+                }
+                female_fighter_string = json.dumps(female_fighter)
+                loaded_female_fighter = json.loads(female_fighter_string)
+                scraped_data_womens.append(loaded_female_fighter)
+                i = i + 1
+            else:
+                if weight_class == "Light":
+                    weight_class = "Light Heavyweight"
+                male_fighter = {
+                    "id": i,
+                    "name": f['fighter'],
+                    "weightClass": weight_class,
+                    "rank": f['rank'],
+                    "age": bio_data['age'],
+                    "height": bio_data['height'],
+                    "reach": bio_data['reach'],
+                    "image": bio_data['image']
+                }
+                male_fighter_string = json.dumps(male_fighter)
+                loaded_male_fighter = json.loads(male_fighter_string)
+                scraped_data_mens.append(loaded_male_fighter)
+                i = i + 1
+
+    with open('female-fighters.json', 'w') as f:
+        f.truncate(0)
+        json.dump(scraped_data_womens, f)
+
+    with open('male-fighters.json', 'w') as f:
+        f.truncate(0)
+        json.dump(scraped_data_mens, f)
+
+
+if __name__ == "__main__":
+    update_all()
